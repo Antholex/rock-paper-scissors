441fb2a5b9448dcb81f94a4205166e674e119dd5
Alex Wozniak <alex@antholex.co.uk>
2019-12-10 18:33:20 +0000

    Create function to play a single round

--- scripts/game.js
+++ scripts/game.js
@@ -12,3 +12,29 @@ function computerPlay() {
       return "Scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  let playerChoice = playerSelection.toLowerCase();
+
+  if (playerChoice == "rock" && computerSelection == "Paper") {
+    return "You lose! Paper beats Rock.";
+  }
+  else if (playerChoice == "rock" && computerSelection == "Scissors") {
+    return "You win! Rock beats Scissors.";
+  }
+  else if (playerChoice == "paper" && computerSelection == "Rock") {
+    return "You win! Paper beats Rock.";
+  }
+  else if (playerChoice == "paper" && computerSelection == "Scissors") {
+    return "You lose! Scissors beats Paper.";
+  }
+  else if (playerChoice == "scissors" && computerSelection == "Paper") {
+    return "You win! Scissors beats Paper.";
+  }
+  else if (playerChoice == "scissors" && computerSelection == "Rock") {
+    return "You lose! Rock beats Scissors.";
+  }
+  else {
+    return "It's a draw!";
+  }
+}
