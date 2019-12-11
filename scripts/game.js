26585331ab494c741db15a4f5d6974bafc8cf1db
Alex Wozniak <alex@antholex.co.uk>
2019-12-11 12:57:08 +0000

    Create function to get users' choice

--- scripts/game.js
+++ scripts/game.js
@@ -13,28 +13,53 @@ function computerPlay() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  let playerChoice = playerSelection.toLowerCase();
+function getUserChoice() {
+  let userChoice = prompt("Please choose Rock, Paper, or Scissors", "");
 
-  if (playerChoice == "rock" && computerSelection == "Paper") {
+  if (userChoice == "" || userChoice == null) {
+    alert("Please enter a valid choice");
+    getUserChoice();
+  }
+  else {
+    userChoice = userChoice.toLowerCase();
+    if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+      return userChoice;
+    }
+    else {
+      alert("Please enter a valid choice");
+      getUserChoice();
+    }
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "Paper") {
     return "You lose! Paper beats Rock.";
   }
-  else if (playerChoice == "rock" && computerSelection == "Scissors") {
+  else if (playerSelection == "rock" && computerSelection == "Scissors") {
     return "You win! Rock beats Scissors.";
   }
-  else if (playerChoice == "paper" && computerSelection == "Rock") {
+  else if (playerSelection == "paper" && computerSelection == "Rock") {
     return "You win! Paper beats Rock.";
   }
-  else if (playerChoice == "paper" && computerSelection == "Scissors") {
+  else if (playerSelection == "paper" && computerSelection == "Scissors") {
     return "You lose! Scissors beats Paper.";
   }
-  else if (playerChoice == "scissors" && computerSelection == "Paper") {
+  else if (playerSelection == "scissors" && computerSelection == "Paper") {
     return "You win! Scissors beats Paper.";
   }
-  else if (playerChoice == "scissors" && computerSelection == "Rock") {
+  else if (playerSelection == "scissors" && computerSelection == "Rock") {
     return "You lose! Rock beats Scissors.";
   }
   else {
     return "It's a draw!";
   }
 }
+
+/*function game() {
+  let playerChoice = getUserChoice();
+  let computerChoice = computerPlay();
+  let outcome = playRound (playerChoice, computerChoice);
+
+  return outcome;
+}*/
