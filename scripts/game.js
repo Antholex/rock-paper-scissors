9244f6b308b5962bc9f51d3787f8fcb36863b871
Alex Wozniak <alex@antholex.co.uk>
2020-11-09 15:42:35 +0000

    Display outcome of round in browser

--- scripts/game.js
+++ scripts/game.js
@@ -23,21 +23,24 @@ function playRound(event) {
   let playerSelection = capitalise(event.target.value);
   let computerSelection = computerPlay();
 
+  const choices = document.querySelector(".choices");
+  const result = document.querySelector(".result");
+
   if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper" ) {
-    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.`);
-    console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+    choices.textContent = `You chose ${playerSelection}. The computer chose ${computerSelection}.`;
+    result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
     playerWin = true;
     return playerWin;
   }
   else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock") {
-    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.`);
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+    choices.textContent = `You chose ${playerSelection}. The computer chose ${computerSelection}.`
+    result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`
     playerWin = false;
     return playerWin;
   }
   else {
-    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.`);
-    console.log("It's a draw!");
+    choices.textContent = `You chose ${playerSelection}. The computer chose ${computerSelection}.`
+    result.textContent = "It's a draw!";
     playerWin = undefined;
     return playerWin;
   }
